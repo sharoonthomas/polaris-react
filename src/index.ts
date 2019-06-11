d61d6c95ae0a74dc8be8e6a3e10b37398e56a5c7
Andrew Musgrave <andrew.musgrave@shopify.com>
2019-06-11 17:58:43 -0400

    Add export backs

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import './configure';
 
 export * from './types';
 export * from './components';
+
+export {createThemeContext} from './utilities/theme-provider';
+export {
+  Props as UnstyledLinkProps,
+  LinkLikeComponent,
+} from './utilities/unstyled-link';
